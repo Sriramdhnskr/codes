fb7259de58f4a49d99882d90ca00a917502b62b8
sriram007-stack <65524363+sriram007-stack@users.noreply.github.com>
2020-06-30 18:06:28 +0530

    Update sum_of_n _natural_nos.c

--- sum_of_n _natural_nos.c
+++ sum_of_n _natural_nos.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-int main()
+int sum(int n)
 {
-    int m;
-    printf("\nEnter the number:");
-    scanf("%d",&m);
     int sum = 0;
     for(int i=1;i<=m;i++)
     {
         sum += i;
     }
-    printf("\nsum of %d natural no.s: %d",m,sum);
+    return sum;
+}
+int main()
+{
+    int m;
+    printf("\nEnter the number:");
+    scanf("%d",&m);
+    printf("\nsum of %d natural no.s: %d",m,sum(m));
     return 0;
 }
